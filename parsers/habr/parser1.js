3cced27ec52e0b7ae7a5432dbffa6538635a3508
Sergey <sergeimurin29@gmail.com>
2022-10-18 12:49:09 +0300

    init item

--- parsers/habr/parser1.js
+++ parsers/habr/parser1.js
@@ -1,18 +1,51 @@
 const AXIOS = require("axios");
-const CHEERIO = require("cheerio")
+const CHEERIO = require("cheerio");
 
-const QUERY = "парсинг";
+const URL = `https://www.ign.com/articles/top-25-best-anime-series-of-all-time`;
 
-const URL = `https://habr.com/ru/search/?q=${QUERY}`;
+const AnimeListItem = {
+    name: "",
+    imageURL: "",
+    description: "",
+    rank: "",
+}
 
 
+AXIOS.get(URL).then(({data}) => {
 
-AXIOS.get(URL).then(data => {
-    const $ = CHEERIO.load(data.data);
-    const headers = [];
-    $(".content-list_posts").each((index, element) => {
-        headers.push($(element).text());
-    })
-    console.log(headers);
-});
+    const $ = CHEERIO.load(data);
+    const articlePage = $(".article-page");
+    const animeList = [];
 
+    const initializeAnimeItem = (node) => {
+        $(node).children("h2").each(( i,el) => {
+            const element = $(el);
+
+            if (element.text().length !== 0) {
+                let animeItem = new Object(AnimeListItem);
+
+                const rankAndName = splitName(element.text());
+                animeItem.name = rankAndName[1];
+                animeItem.rank = rankAndName[0];
+
+                const imgElement = $(element.next().children().children().html())
+                animeItem.imageURL = imgElement.attr("src");
+
+                animeList.push(animeItem);
+            }
+        })
+    }
+
+    const findSrc = (node) => {
+        console.log(node.find("img"))
+    }
+
+    const splitName = (str) => {
+        return str.split(". ");
+    }
+
+    initializeAnimeItem(articlePage);
+    console.log(animeList);
+
+
+}).catch((error) => console.error(error));
