98b4ab39a97b18650c8e091c5ce54b003546a433
Sergey <sergeimurin29@gmail.com>
2022-10-18 08:38:01 +0300

    Init

---
+++ parsers/habr/options.config.js
@@ -0,0 +1,9 @@
+export const OPTIONS_CONFIG = {
+    query: "q",
+    orderBy: {
+        index: "order_by",
+        relevance: "relevance",
+        date: "date",
+        rating: "rating",
+    }
+}
---
+++ parsers/habr/selectors.config.js
@@ -0,0 +1,4 @@
+export const SELECTORS = {
+    paginationMaxPage: "div.tm-pagination__page-group:nth-child(3) > a:nth-child(1)",
+
+}
