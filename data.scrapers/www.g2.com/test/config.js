025ec10e6e9aa807c5fddbed15737bfe264beb09
Sergey <sergeimurin29@gmail.com>
2022-11-18 19:19:37 +0300

    commit

---
+++ data.scrapers/www.g2.com/test/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    selectors: {
+        search_field: {
+            input_selector: ".ajax-search-field",
+            popup_selector: ".tt-menu",
+            section_selector: ".tt-dataset",
+            categories_selector: ".tt-dataset-categories",
+            products_selector: ".tt-dataset-products",
+            discussions_selector: ".tt-dataset-discussions",
+            articles_selector: ".tt-dataset-articles",
+            vendors_selector: ".tt-dataset-vendors",
+            section_header_selector: "h3",
+            section_list_selector: ".search-result-link",
+
+        }
+    }
+}
---
+++ data.scrapers/www.g2.com/test/test.js
@@ -0,0 +1,95 @@
+const {remote} = require("webdriverio");
+const config = require("./config");
+const URL = "https://www.g2.com/";
+
+const run = async () => {
+    const browser = await remote({
+        capabilities: {
+            browserName: "chrome",
+        }
+    })
+    await browser.url(URL);
+    //await openFirstUrlWithGoogle(browser, URL);
+    console.log();
+
+    setTimeout(async () => {
+        await search("car", browser);
+    }, 5000)
+
+    /*
+        const categories = await browser
+            .$(config.selectors.search_field.categories_selector);
+
+        await categories.waitForDisplayed();
+        let text = await categories.getText();*/
+
+
+    /*setTimeout(() => {
+        browser.deleteSession();
+    }, 80000);*/
+};
+
+
+const search = async (query, browser) => {
+    await setSearch(browser, config.selectors.search_field.input_selector, query);
+    await wait(browser, config.selectors.search_field.popup_selector);
+    await getSearchResult(browser, config.selectors.search_field.popup_selector);
+}
+
+const setSearch = async (browser, selector, query) => {
+    const searchFieldElement = await browser.$(selector);
+    await searchFieldElement.click();
+    await searchFieldElement.setValue(query);
+}
+
+const wait = async (browser, selector) => {
+    const element = await browser.$(selector);
+    await element.waitForDisplayed({timeout: 10000});
+    return element;
+}
+
+const getSearchResult = async (browser, selector) => {
+    await getSearchResultSections(browser, config.selectors.search_field.section_selector);
+    await cookiesHandler(browser);
+
+}
+
+const getSearchResultSections = async (browser, selector) => {
+    const resultSections = [];
+    const elements = await browser.$$(selector);
+    const sectionHeaderElements = [];
+
+    for (let element of elements) {
+        const sectionHeaderElement = await element.$(config.selectors.search_field.section_header_selector);
+        await sectionHeaderElement.scrollIntoView();
+        const sectionListElements = await element.$$(config.selectors.search_field.section_list_selector);
+        await sectionListElements[1].click();
+        break;
+    }
+}
+
+const openFirstUrlWithGoogle = async (browser, query) => {
+    await browser.url("google.com");
+    const input = await wait(browser, "input");
+    await input.setValue("g2 crowd");
+    const button = await wait(browser, ".gNO89b");
+    await button.click();
+    const searchOutput = await wait(browser, "#search");
+    await searchOutput.$("a").click();
+};
+
+const cookiesHandler = async (browser) => {
+    const allCookies = await browser.getCookies();
+    const cookieNames = await allCookies.map((cookie) => {
+        return cookie.name;
+    });
+    setTimeout(async () => {
+        await browser.execute("window.localStorage.clear()");
+        await browser.deleteCookies(cookieNames);
+        await browser.url(await browser.getUrl());
+    }, 10000);
+
+}
+
+
+run();
